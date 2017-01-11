30105dccc06678d4f3a55562326b9ba9040484c4
PD <dave.sfsu@gmail.com>
2017-01-11 01:02:58 -0800

    Test

--- TipCalculatorProper/ViewController.swift
+++ TipCalculatorProper/ViewController.swift
@@ -69,7 +69,6 @@ class ViewController: UIViewController {
         
         let perPerson = total / numOfPeople
         
-        
         let formatter = NumberFormatter()
         formatter.numberStyle = NumberFormatter.Style.decimal
         formatter.minimumFractionDigits = 2
